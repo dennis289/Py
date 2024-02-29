fadaeb29021fa07ce33b76ad155a3e8d3e69da5c
dennis289 <edenismacharia7@gmail.com>
2024-02-29 10:04:08 +0300

    classes

--- Classes/banking.py
+++ Classes/banking.py
@@ -35,18 +35,3 @@ bank.deposit()
 bank.withdraw()
 bank.check_balance()
 
-# class Temparature:
-#     def __init__(self,degree,farenheit) -> None:
-#         self.degree=degree
-#         self.farenheit=farenheit
-#     def Degreeconvert(self):
-#         degrees= self.farenheit/1.8+32
-#         print("the degree euivalent is:",degrees)
-#     def Farenheitconvert(self):
-#         farenheit= self.degree* 1.8 -32
-#         print("The farenheit equivalent is:",farenheit )
-# Degrees= float(input("Enter the degrees to be converted: "))
-# Farenheits= float(input("Enter the ferenheit measure to be converted: "))
-# temp= Temparature(Degrees,Farenheits)
-# temp.Degreeconvert()
-# temp.Farenheitconvert()
